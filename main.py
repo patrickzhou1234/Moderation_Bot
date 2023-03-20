1ec4c253480e9db44150b9dd636aee58eaebe244
patrickzhou1234 <patrickchang44543@gmail.com>
2023-03-20 08:05:32 -0700

    addedadmin

--- main.py
+++ main.py
@@ -37,17 +37,20 @@ async def on_message_delete(message):
     await discord.utils.get(message.guild.channels, name='delete-logs').send(log_message)
 
 @slash.slash(name='delete', description='Clear the chat', options=[create_option(name='amount', description='The number of messages to delete', option_type=4, required=False)])
+@commands.has_permissions(administrator=True)
 async def _clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
     clearmsg = f"**{ctx.author}** cleared **{amount}** messages in **{ctx.channel}**."
     await ctx.reply(clearmsg)
 
 @slash.slash(name='kick', description='Kick a member', options=[create_option(name='member', description='The member to kick', option_type=6, required=True), create_option(name='reason', description='The reason for kicking the member', option_type=3, required=False)])
+@commands.has_permissions(administrator=True)
 async def _kick(ctx, member: discord.Member, reason=None):
     await member.kick(reason=reason)
     await ctx.reply(f'{member} has been kicked.')
 
 @slash.slash(name='ban', description='Ban a member', options=[create_option(name='member', description='The member to ban', option_type=6, required=True), create_option(name='reason', description='The reason for banning the member', option_type=3, required=False)])
+@commands.has_permissions(administrator=True)
 async def _ban(ctx, member: discord.Member, reason=None):
     await member.ban(reason=reason)
     await ctx.reply(f'{member} has been banned.')
